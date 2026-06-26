84a0ea48b4c70aa1a0c8f0b74f6cedbbe0748b9e
Subham <subham7031979@gmail.com>
2026-06-26 16:10:53 +0530

    2026-06-26

--- advance-programming/simple-rust/src/main.rs
+++ advance-programming/simple-rust/src/main.rs
@@ -19,7 +19,37 @@ fn main() {
 
     // Constants [const variable name with caps type = value {nothing is optional}]
     const MAX_VALUE:i32 = 200;
-    println!("Max value : {MAX_VALUE}")
+    println!("Max value : {MAX_VALUE}");
 
+    // Code block
+
+    let code_block_value = {
+        format!("Hello Everyone")
+    };
+
+    println!("Code block Value : {code_block_value}");
+
+    let num = 10;
+    print_number(num);
+
+    // num += 12;
+    println!("Number : {num}");
+
+    let (num1,num2,num3) = tuple_value(1,4, 8);
+    println!("num1 : {num1} | num2 : {num2} | num3 : {num3}");
 
 } 
+
+// Mutability in Function param
+fn print_number(mut x:i32) {
+    x += 10;
+    println!("X : {x}");
+}
+
+fn tuple_value(num1:i32, num2:i32, num3:i32) -> (i32,i32,i32) {
+    (num1 + num2 + num3, num1 * num2 * num3, (num1 + num2 + num3) / 3)
+}
+
+// Shadowing Function
+
+
