8a2e780c91fd912de700b1ac59ab8a90ff486ab4
Subham <subham7031979@gmail.com>
2026-06-27 16:27:45 +0530

    match in rust

--- advance-programming/simple-rust/src/main.rs
+++ advance-programming/simple-rust/src/main.rs
@@ -42,8 +42,9 @@ fn main() {
     // Shadowing 
     let x = 13;
     let x_value = shadowing_fun(x);
-    print!("X Value in Shadowing : {x_value}");
+    print!("X Value in Shadowing : {x_value}\n");
 
+    condition_fun();
 } 
 
 // Mutability in Function param
@@ -62,4 +63,60 @@ fn shadowing_fun(x: i32) -> i32 {
     x += 30;
     x
 }
+
+fn condition_fun() {
+    // Simple Condition 
+    let mark = 70;
+    let mut grade = 'A';
+
+    if mark >= 90 {
+        grade = 'A';
+    } else if mark >= 80 {
+        grade = 'B';
+    } else if mark >= 70 {
+        grade = 'C';
+    } else if mark >= 50 {
+        grade = 'D';
+    } else {
+        grade = 'F';
+    }
+
+    println!("Mark : {mark} | Grade : {grade}");
+
+    // Short form Condition
+    let mark = 90;
+    let grade = if mark >= 90 {
+        'A'
+    } else if mark >= 80 {
+        'B'
+    } else if mark >= 70 {
+        'C'
+    } else if mark >= 50 {
+        'D'
+    } else {
+        'F'
+    };
+
+    println!("Mark : {mark} | Grade : {grade}");
+}
+
+// Match Condition [Like Switch Case] 
+fn match_fun() {
+    let marks = 90;
+    let mut grade = 'N';
+
+    match marks {
+        90..=100 => grade = 'A', // condition [Pattern Matching] => after match code need to run
+        80..=89 => grade = 'B',
+        50..=79 => grade = 'C',
+        _=> grade = 'F', // if not matched [default]
+    }
+
+    println!("Mark : {marks} | Grade : {grade}");
+}
+
+
+
+
+
 
