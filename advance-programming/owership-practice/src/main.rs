86471743cb7933aca0cf6d984dca1ad2439eb035
Subham <subham7031979@gmail.com>
2026-06-30 16:15:34 +0530

    borrow practice

--- advance-programming/owership-practice/src/main.rs
+++ advance-programming/owership-practice/src/main.rs
@@ -22,6 +22,50 @@ fn main() {
     ref_1.push(60);
     println!("Reference Data : {:?}",vec_1);
 
+    // Function that immutably borrow values
+    let vec_1 = vec![4,6,8,90];
+    let ref_1 = &vec_1;
+    borrow_reference(ref_1);
+    println!("Vector Value : {:?}",vec_1);
+
+
+    // function that mutably borrow values
+    let mut vec_1 = vec![09,45,76,132,76];
+    let ref_1 = &mut vec_1;
+    take_and_return_ownership(ref_1);
+    println!("Orginal Vector Value : {:?}",vec_1);
+
+    //  Functions that do not uses borrow but returns it
+    // let vec_2 = vec![4,8,9,2,68,34689];
+    // let ref_2 = return_reference(vec_2);
+    // println!("Reference 2 : {:?}",ref_2);
+
+    //  Functions that uses mixed type of borrow
+    let mut vec_1 = vec![2,45,1211,7,3,2,64];
+    let subject = "hello Subham Gupta";
+    mixed_borrow(&subject, &mut vec_1);
+
+}
+
+fn mixed_borrow(subject: &str, vec: &mut Vec<i32>) {
+    println!("Subject value '{}' Vector Value {:?}", subject, vec);
+    vec.push(subject.len() as i32);
+    println!("Subject value '{}' Vector Value {:?}", subject, vec);
+}
+
+// Not valid variable
+// fn return_reference(vec: Vec<i32>) -> &Vec<i32> {
+//     &vec
+// }
+
+// not valid variable
+// fn return_reference() -> &Vec<i32> {
+//     let vec = vec![33,5,2,6,2];
+//     &vec
+// }
+
+fn take_and_return_ownership(vec:&mut Vec<i32>) {
+    vec.push(50);
 }
 
 fn take_and_give_ownership(mut vec: Vec<i32>) -> Vec<i32> {
@@ -36,3 +80,9 @@ fn give_ownership() -> Vec<i32> {
 fn take_owership(vec: Vec<i32>) {
     println!("Print Vec : {:?}",vec);
 }
+
+fn borrow_reference(vec: &Vec<i32>) {
+    println!("Reference of Vector : {:?}",vec);
+}
+
+
